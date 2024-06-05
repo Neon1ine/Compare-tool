9ac9e9a9dbf0dc1f5c41dceecb0822f27b300c27
Neon1ine <azeemin1548@yandex.ru>
2024-06-05 19:12:12 +0300

    fix format String

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -8,16 +8,23 @@ import java.util.List;
 public class Formatter {
 
     public static String convert(List<List<String>> outputList, String format) throws Exception {
-        format = format.toLowerCase().trim();
-        if (format.isEmpty() || format.equals("[]")) {
-            format = "stylish";
-        }
-        String result = switch (format) {
+        String result = switch (fixFormat(format)) {
             case "plain" -> Plain.getString(outputList);
             case "json" -> Json.getString(outputList);
             case "stylish" -> Stylish.getString(outputList);
             default -> throw new IllegalStateException("Unexpected format: " + format);
         };
+        return result;
+    }
+
+    private static String fixFormat(String format) {
+        String result = format.toLowerCase().trim();
+        if (result.startsWith("[") && result.endsWith("]")) {
+            result = result.substring(1, result.length() - 1);
+        }
+        if (result.isEmpty()) {
+            result = "stylish";
+        }
         return result;
     }
 
